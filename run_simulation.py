878c2c702912bbc43613074ccf5eb22c9c4b5e84
ethan0922 <ethany8362@gmail.com>
2022-11-10 18:35:20 +0000

    Nov 10, 2022, 6:35 PM

--- run_simulation.py
+++ run_simulation.py
@@ -1,6 +1,6 @@
 from simulation.simulation import simulate
 
-pow_cons, anx = simulate(10, "./example_scenarios/scenario3.txt")
+pow_cons, anx = simulate(10, "./example_scenarios/scenario1.txt")
 
 print(pow_cons)
 print(anx)

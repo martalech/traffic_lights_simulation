1e5d23660744ab7c4971f1c84458dd81e23e2d67
Tianchi Dong <100702745+DTC320@users.noreply.github.com>
2022-11-11 11:02:17 +0100

    Create Light_intensity.py
    It's a way to caculate the light intensity

---
+++ ui_model/Light_intensity.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+
+class Intensity(x, y):
+  self.light_hight = 3000 #mm
+  self.person_high = 180 #mm
+  
+  self.x = x
+  self.y = y
+  
+  self.threshold = 20 #lux
+  self. 
+  
+  
+  def get_intensity(x1,y1,x2,y2):
+      distance =
+  

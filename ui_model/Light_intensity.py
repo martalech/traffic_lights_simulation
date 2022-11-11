21222ea915973885a9c441d26537e12c42930e50
Tianchi Dong <100702745+DTC320@users.noreply.github.com>
2022-11-11 11:19:58 +0100

    Update Light_intensity.py

--- ui_model/Light_intensity.py
+++ ui_model/Light_intensity.py
@@ -2,17 +2,33 @@ import numpy as np
 
 
 
-class Intensity(x, y):
-  self.light_hight = 3000 #mm
-  self.person_high = 180 #mm
+class Intensity:
+  def __init__(self, )
+    self.light_hight = 3000 #mm
+    self.person_high = 180 #mm
   
-  self.x = x
-  self.y = y
+    self.x = x
+    self.y = y
   
-  self.threshold = 20 #lux
-  self. 
+    self.threshold = 20 #lux
+    self.Itenstiy_max = 1000
   
   
   def get_intensity(x1,y1,x2,y2):
-      distance =
+      #(x1,y1) is the coordinate of the light1
+      #(x2,y2) is the coordinate of the light2 
+   
+      distance1 =  math.sqrt((x1 - self.x) * (x1 - self.x) + (y1 - self.y) * (y1 - self.y)) #the distance form the light 1
+      distance2 =  math.sqrt((x2 - self.x) * (x2 - self.x) + (y2 - self.y) * (y2 - self.y)) #the distance from the light 2
+      
+      light_intensity_1 = 
+      
+      
+      return distance1, distance2
+    
+  
+    
+      
+  
+  
   

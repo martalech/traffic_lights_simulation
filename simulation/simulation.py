8cf2361357d901007f8becab91e2f52de98ab3e0
niewiadomskam <51495809+niewiadomskam@users.noreply.github.com>
2022-11-18 13:46:32 +0100

    Convert gathered energy into kilowats per hour

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -25,7 +25,7 @@ def simulate(people_number, scenario_file_name):
                 street_map.adjust_traffic(shift_factor)
             if time.current_hour != time.previous_hour and time.incrementer == 0: # change of hour
                 # save data for previous hour
-                power_consumptions.append(sum(energy)) # TODO: make it per hour? or per minute maybe ?
+                power_consumptions.append(sum(energy) / (time.resolution * 1000) ) # kilo wats per hour
                 energy = []
                 anxiety.append(sum(current_anxiety)/len(current_anxiety))  
                 current_anxiety = []
@@ -34,7 +34,7 @@ def simulate(people_number, scenario_file_name):
             for l in lights:
                 l.adjust_light(street_map.people)
                 l.calculate_energy()
-                energy.append(l.power) # TODO: here multiply by some value for power depending on dimming...
+                energy.append(l.power) 
             # gather data for anxiety:
             for p in street_map.people:
                 anx = p.calculate_anxiety(lights)
@@ -42,19 +42,9 @@ def simulate(people_number, scenario_file_name):
                       
     except:
         # handle last hour
-        power_consumptions.append(sum(energy))
+        power_consumptions.append(sum(energy)/ (time.resolution * 1000))
         anxiety.append(sum(current_anxiety)/len(current_anxiety))  
 
-    # output result to data folder
-    # default directory name = scenario name + power ratio + time
-    dir = "./data/" + scenario_file_name.split("/")[-1].split(".")[0] +'_'+str(lights[0].ratio)
-    time_now  = datetime.datetime.now().strftime('_%Y%m%d_%H%M%S') 
-    dir = dir + time_now
-    if os.path.exists(dir):
-        raise FileExistsError
-    else:
-        for light in lights:
-            light.output_data(dir)
 
     return power_consumptions, anxiety
 
